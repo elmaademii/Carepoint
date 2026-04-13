b653ca721aa6c14a185b6231b5adf673268d96aa
Elma-coder1 <elma.ademi@student.uni-pr.edu>
2026-04-14 00:31:04 +0200

    Updated contact page

---
+++ includes/navbar.php
@@ -0,0 +1,27 @@
+<div class="top-bar">
+    <div class="container top-bar-content">
+        <div class="top-left">
+            <span><i class="fa-solid fa-phone"></i> +383 (0) 49 513 513</span>
+            <span><i class="fa-solid fa-envelope"></i> info@carepoint.com</span>
+        </div>
+        <div class="top-right">
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+    </div>
+</div>
+
+<header class="main-header">
+    <div class="container header-content">
+        <div class="logo">CarePoint</div>
+
+        <nav class="main-nav">
+            <a href="ballina.php" class="active">Ballina</a>
+            <a href="about.php">Rreth CarePoint</a>
+            <a href="services.php">Shërbimet</a>
+            <a href="staff.php">Stafi</a>
+            <a href="kontact.php">Kontakt</a>
+        </nav>
+    </div>
+</header>
